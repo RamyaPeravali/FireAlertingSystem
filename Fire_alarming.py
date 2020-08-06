0dc2fa57a27234cdeeae1fbc1b37fea33b44d9c1
Ramya Peravali <52354391+RamyaPeravali@users.noreply.github.com>
2020-08-06 12:35:04 +0530

    Smoke code added

--- Fire_alarming.py
+++ Fire_alarming.py
@@ -1,5 +1,5 @@
-#pip install ibmiotf 
-#git clone https://github.com/szazo/DHT11_python.git #(dht11 library)
+#pip install ibmiotf (for ibm platform)
+#git clone https://github.com/szazo/DHT11_python.git (dht11 library)
 
 import RPi.GPIO as GPIO
 import dht11
@@ -25,19 +25,22 @@ GPIO.setup(16,GPIO.OUT)
 GPIO.setup(21,GPIO.IN)
 
 SensorInstance = dht11.DHT11(pin=21)
-
+adc = MCP3208()
+#while True:
+    #SensorInstance1 = adc.read(0)
+    #print('adc[0]', SensorInstance1)
 #Initialize the device client.
 try:
     deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod, "auth-token": authToken}
     deviceCli = ibmiotf.device.Client(deviceOptions) #connect to ibmm iot platform
 except Exception as e:
     print("Caught exception connecting device: %s" % str(e))
-    sys.exit()#terminate program
+    sys.exit() #terminate program
 
 def mycommandCallback(cmd):
     print("command received: %s" % cmd.data['command'])
     s=str(cmd.data['command'])
-    print ("s")
+    print(s)
     if(s=="LIGHTON"):
         GPIO.output(16,True)
     elif(s=="LIGHTOFF"):
@@ -47,21 +50,32 @@ deviceCli.connect()
 while True:
     SensorData=SensorInstance.read()
     SensorInstance1=adc.read(0)
+
     if SensorDats.is_valid():
-        T=SensorData.temperature
-        H=SensorData.humidity
-        data={'temperature':T,'Humidity':H}
+        T = SensorData.temperature
+        H = SensorData.humidity
+        data={'Temperature' : T,'Humidity' : H}
         def myOnpublishCallback():
-            print ("Published Temperature=%s C" %T,"Humidity=%s %%" %H, "to IBM Watson")
-        success=deviceCli.publishEvent("MCP3208","json",data1,qos=0,on_publish=myOnPublishCallback)
+            print ("Published Temperature = %s C" %T,"Humidity = %s %%" %H, "to IBM Watson")
+        success = deviceCli.publishEvent("DHT11","json", data, qos=0, on_publish=myOnPublishCallback)
         if not success:
             print("Not connected to IoTF")
         time.sleep(2)
     else:
         print ("SensorData Invalid")
+    if  SensorInstance1.is_valid():
+        S = SensorInstance1.smoke()
+        #H = SensorData.humidity
+        data1 = {'Smoke' : S}
+        def myOnPublicCallback():
+            print("Published Smoke = %s " %S, "to IBM Watson")
+        success = deviceCli.publishEvent("MCP3208","json",data1,qos=0,on_publish=myOnPublishCallback) 
+        if not success:
+            print("Not Connected to ToTF")
+        time.sleep(2)
+    else:
+        print("SensorData Invalid")
     deviceCli.commandCallbackk=mycommandCallback
 
 #Disconnect the device and application from the cloud
 deviceCli.disconnect()
-
-
